c37d25ef5fda6b63713b49c3514fd822d05f2786
Kayla Robinson <kayshae1125@gmail.com>
2018-01-10 11:57:28 -0500

    second change to try and recreate issue seen before

--- newGitConsoleApp/newGitConsoleApp/Program.cs
+++ newGitConsoleApp/newGitConsoleApp/Program.cs
@@ -10,6 +10,7 @@ namespace newGitConsoleApp
             Console.WriteLine("What is your name?");
             var username = Console.ReadLine(); 
             Console.WriteLine("Nice to meet you, " + username + "!");
+            Console.WriteLine("New Line.. what happens now?");
         }
     }
 }
