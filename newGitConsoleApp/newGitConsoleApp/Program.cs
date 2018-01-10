d8b06221d0a8765288070e30050314287518ba15
Kayla Robinson <kayshae1125@gmail.com>
2018-01-10 11:19:29 -0500

    created new file and created introduction.

---
+++ newGitConsoleApp/newGitConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace newGitConsoleApp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var username = Console.ReadLine();            
+        }
+    }
+}
