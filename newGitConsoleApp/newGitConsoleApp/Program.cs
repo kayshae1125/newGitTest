f8bcc480a5af0a49d605899fd6288377884b486f
Kayla Robinson <kayshae1125@gmail.com>
2018-01-10 12:03:24 -0500

    Try 3

--- newGitConsoleApp/newGitConsoleApp/Program.cs
+++ newGitConsoleApp/newGitConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace newGitConsoleApp
             var username = Console.ReadLine(); 
             Console.WriteLine("Nice to meet you, " + username + "!");
             Console.WriteLine("New Line.. what happens now?");
+            Console.WriteLine("Let's add another line"); 
         }
     }
 }
