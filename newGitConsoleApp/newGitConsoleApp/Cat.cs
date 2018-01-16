025ef182d4ecf9859ae5413c02f954af42b048ad
Kayla Robinson <kayshae1125@gmail.com>
2018-01-16 16:55:47 -0500

    created cat class, meow, and age

---
+++ newGitConsoleApp/newGitConsoleApp/Cat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace newGitConsoleApp
+{
+    class Cat
+    { 
+        public string Name = string.Empty;
+        public int Age = 0; 
+        public void Hiss()
+        {
+            Console.WriteLine("Hiss"); 
+        }
+        
+        public static void Meow()
+        {
+            Console.WriteLine("Meow.");
+        }
+
+        public int GetAge()
+
+        {
+            return Age;
+        }
+    }
+}
--- newGitConsoleApp/newGitConsoleApp/Program.cs
+++ newGitConsoleApp/newGitConsoleApp/Program.cs
@@ -5,14 +5,19 @@ namespace newGitConsoleApp
     class MainClass
     {
         public static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Hello World!");
             Console.WriteLine("What is your name?");
+            //User interacts with console by inputting information and pressing enter.  
             var username = Console.ReadLine(); 
             Console.WriteLine("Nice to meet you, " + username + "!");
-            Console.WriteLine("New Line.. what happens now?");
-            Console.WriteLine("Let's add another row"); 
-            Console.ReadKey(); 
+            //Here comes the cat 
+            Cat cCat = new Cat();
+            cCat.Hiss();
+            Cat.Meow();
+            cCat.Age = 10; 
+            Console.WriteLine(cCat.GetAge());  
+            Console.Read(); 
         }
-    }
+    }  
 }
